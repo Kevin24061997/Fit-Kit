d9d0dda6d833b62f4ed92f9510e17849667de6b2
SirKaktor <juliuststelldinger@web.de>
2023-09-04 14:24:45 +0200

    Refactor App.js v0.1

---
+++ fit-kit/src/EinstellungScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export function EinstellungScreen() {
+  return (
+    <View>
+      <Text>Einstellung</Text>
+      {/* Weitere Inhalte für den Einstellung-Bildschirm */}
+    </View>
+  );
+}
