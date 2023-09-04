08ed76a7b4db9bccbf81c394b14d2c634633f947
Kevin24061997 <115979444+Kevin24061997@users.noreply.github.com>
2023-09-04 12:51:46 +0200

    Timer global / Alert

--- fit-kit/App.js
+++ fit-kit/App.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect, useCallback, useContext } from 'react';
-import { View, Text, Button, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect, useContext } from 'react';
+import { View, Text, Button, ScrollView, StyleSheet, TouchableOpacity, Alert } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// TimerContext erstellen
 const TimerContext = React.createContext();
 
 const styles = StyleSheet.create({
@@ -135,27 +134,18 @@ const Stack = createNativeStackNavigator();
 
 function TimerHeader() {
   const navigation = useNavigation();
-  const [seconds, setSeconds] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
-  const [übungsBlöcke, setÜbungsBlöcke] = useState([]);
-
-  function navigateToEinzelÜbung() {
-    navigation.navigate('EinzelÜbung');
-  }
-
-  const addÜbungsBlock = () => {
-    // Hier füge einen neuen Übungsblock hinzu, z.B. "Übung X"
-    const newÜbungsBlöcke = [...übungsBlöcke, `Übung`];
-    setÜbungsBlöcke(newÜbungsBlöcke);
-  };
-
-  const removeÜbungsBlock = () => {
-    // Hier entferne den letzten Übungsblock
-    if (übungsBlöcke.length > 0) {
-      const newÜbungsBlöcke = übungsBlöcke.slice(0, übungsBlöcke.length - 1);
-      setÜbungsBlöcke(newÜbungsBlöcke);
-    }
-  };
+  const { timer, setTimer } = useContext(TimerContext);
+  const [seconds, setSeconds] = useState(timer.seconds || 0);
+  const [isRunning, setIsRunning] = useState(timer.isRunning || false);
+  const [übungsBlöcke, setÜbungsBlöcke] = useState([
+    'Übung',
+    'Übung',
+    'Übung',
+    'Übung',
+    'Übung',
+    'Übung',
+    'Übung',
+  ]);
 
   useEffect(() => {
     let timer;
@@ -173,43 +163,122 @@ function TimerHeader() {
     };
   }, [isRunning]);
 
-  const startStopTimer = useCallback(() => {
-    setIsRunning((prevIsRunning) => !prevIsRunning);
-  }, []);
+  const startTimer = () => {
+    setIsRunning(true);
+    setTimer({ seconds, isRunning: true });
+  };
+
+  const stopTimer = () => {
+    setIsRunning(false);
+    setTimer({ seconds, isRunning: false });
+  };
+
+  const handleTimerButtonPress = () => {
+    if (isRunning) {
+      Alert.alert(
+        'Training beenden?',
+        'Sind Sie sicher, dass Sie das Training beenden möchten?',
+        [
+          {
+            text: 'nein',
+            onPress: () => {},
+          },
+          {
+            text: 'ja',
+            style: 'cancel',
+            onPress: () => {
+              stopTimer();
+            },
+          },
+        ]
+      );
+    } else {
+      startTimer();
+    }
+  };
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalId);
+    }
+
+    setTimer({ seconds, isRunning });
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isRunning, seconds, setTimer]);
 
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => (
-        <Button
-          title={isRunning ? 'Stop' : 'Starten'}
-          onPress={startStopTimer}
-        />
+        <Button title={isRunning ? 'Stop' : 'Starten'} onPress={handleTimerButtonPress} />
       ),
       headerTitle: () => (
         <View style={styles.headerContainer}>
-          <Text style={{ fontSize: 20, color: 'white' }}>{seconds} seconds</Text>
+          <Text style={styles.timerText}>{formatTime(seconds)}</Text>
         </View>
       ),
     });
-  }, [navigation, startStopTimer, isRunning, seconds]);
+  }, [navigation, handleTimerButtonPress, isRunning, seconds]);
+
+  const navigateToEinzelÜbung = () => {
+    navigation.navigate('EinzelÜbung');
+  };
+
+  const addÜbungsBlock = () => {
+    // Hier füge einen neuen Übungsblock hinzu, z.B. "Übung X"
+    const newÜbungsBlöcke = [...übungsBlöcke, `Übung`];
+    setÜbungsBlöcke(newÜbungsBlöcke);
+  };
+
+  const removeÜbungsBlock = () => {
+    // Hier entferne den letzten Übungsblock
+    if (übungsBlöcke.length > 0) {
+      const newÜbungsBlöcke = übungsBlöcke.slice(0, übungsBlöcke.length - 1);
+      setÜbungsBlöcke(newÜbungsBlöcke);
+    }
+  };
+
+  // Funktion, um eine Zahl in das Format "00" zu konvertieren
+  const formatNumber = (number) => {
+    return number < 10 ? `0${number}` : `${number}`;
+  };
+
+  // Funktion zur Umwandlung der Sekunden in "00:00:00" Format
+  const formatTime = (totalSeconds) => {
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+    return `${formatNumber(hours)}:${formatNumber(minutes)}:${formatNumber(seconds)}`;
+  };
 
   return (
     <ScrollView style={styles.ÜbungsBlockContainer}>
       <View style={styles.buttonContainer1}>
-        <Text style={styles.buttonText1} onPress={addÜbungsBlock}>Hinzufügen</Text>
-        <Text style={styles.buttonText1} onPress={removeÜbungsBlock}>Entfernen</Text>
+        <Text style={styles.buttonText1} onPress={addÜbungsBlock}>
+          Hinzufügen
+        </Text>
+        <Text style={styles.buttonText1} onPress={removeÜbungsBlock}>
+          Entfernen
+        </Text>
       </View>
       <TouchableOpacity onPress={navigateToEinzelÜbung}>
         {übungsBlöcke.map((übungsBlock, index) => (
-        <View style={styles.ÜbungsBlock} key={index}>
-          <Text style={styles.infoText}>{`${index + 1}: ${übungsBlock}`}</Text>
-        </View>
+          <View style={styles.ÜbungsBlock} key={index}>
+            <Text style={styles.infoText}>{`${index + 1}: ${übungsBlock}`}</Text>
+          </View>
         ))}
       </TouchableOpacity>
     </ScrollView>
   );
 }
-
 
 // Timer #######################################################################
 
@@ -222,7 +291,84 @@ function EinstellungScreen() {
   );
 }
 
+// Einzel Übung #########################################################
+
 function EinzelÜbungScreen() {
+  const { timer, setTimer } = useContext(TimerContext);
+  const navigation = useNavigation();
+  const [seconds, setSeconds] = useState(timer.seconds || 0);
+  const [isRunning, setIsRunning] = useState(timer.isRunning || false);
+
+  // Funktion, um eine Zahl in das Format "00" zu konvertieren
+  const formatNumber = (number) => {
+    return number < 10 ? `0${number}` : `${number}`;
+  };
+
+  // Funktion zur Umwandlung der Sekunden in "00:00:00" Format
+  const formatTime = (totalSeconds) => {
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+    return `${formatNumber(hours)}:${formatNumber(minutes)}:${formatNumber(seconds)}`;
+  };
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalId);
+    }
+
+    setTimer({ seconds, isRunning });
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isRunning, seconds, setTimer]);
+
+  const handleTimerButtonPress = () => {
+    if (isRunning) {
+      Alert.alert(
+        'Training beenden?',
+        'Sind Sie sicher, dass Sie das Training beenden möchten?',
+        [
+          {
+            text: 'nein',
+            onPress: () => {},
+          },
+          {
+            text: 'ja',
+            style: 'cancel',
+            onPress: () => {
+              setIsRunning(false);
+              setTimer({ seconds, isRunning: false });
+            },
+          },
+        ]
+      );
+    } else {
+      setIsRunning(true);
+      setTimer({ seconds, isRunning: true });
+    }
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Button title={isRunning ? 'Stop' : 'Starten'} onPress={handleTimerButtonPress} />
+      ),
+      headerTitle: () => (
+        <View style={styles.headerContainer}>
+          <Text style={styles.timerText}>{formatTime(seconds)}</Text>
+        </View>
+      ),
+    });
+  }, [navigation, handleTimerButtonPress, isRunning, seconds]);
+
   return (
     <View>
       <Text>Übung</Text>
@@ -230,6 +376,8 @@ function EinzelÜbungScreen() {
     </View>
   );
 }
+
+// Einzel Übung #########################################################
 
 // Page ######################################################################
 
@@ -320,30 +468,40 @@ const Page = ({ navigation }) => {
 // Page #################################################################
 
 function App() {
+  const [timer, setTimer] = useState({ seconds: 0, isRunning: false });
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Page">
-        <Stack.Screen name="Training" component={Page} options={{
-          headerStyle: {
-            backgroundColor: 'black',
-          },
-          headerTitleStyle: {
-            color: 'white', 
-          },
-        }} />
-        <Stack.Screen name="Timer" component={TimerHeader} 
-        options={{
-          headerStyle: {
-            backgroundColor: 'black',
-          },
-          headerTitleStyle: {
-            color: 'white', 
-          },
-        }} />
-        <Stack.Screen name="EinzelÜbung" component={EinzelÜbungScreen} />
-        <Stack.Screen name="Einstellung" component={EinstellungScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <TimerContext.Provider value={{ timer, setTimer }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Page">
+          <Stack.Screen name="Training" component={Page} options={{
+            headerStyle: {
+              backgroundColor: 'black',
+            },
+            headerTitleStyle: {
+              color: 'white', 
+            },
+          }} />
+          <Stack.Screen
+            name="Timer"
+            component={TimerHeader}
+            options={{
+              headerStyle: {
+                backgroundColor: 'black',
+              },
+            }}
+          />
+          <Stack.Screen name="EinzelÜbung" component={EinzelÜbungScreen}
+          options={{
+            headerStyle: {
+              backgroundColor: 'black',
+            },
+          }}
+           />
+          <Stack.Screen name="Einstellung" component={EinstellungScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </TimerContext.Provider>  
   );
 }
 
